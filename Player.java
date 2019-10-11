63e1e4dcfa7de8d8baee5bf1e5e19e22452060ee
sohumbhole <48001832+sohumbhole@users.noreply.github.com>
2019-10-10 21:10:35 -0500

    Add files via upload

---
+++ Player.java
@@ -0,0 +1,49 @@
+package com.example.loacalstoragetest;
+
+public class Player {
+    private String name;
+    private int gamesWon, gamesLost, setsWon, setsLost;
+    public Player(String nameParam){
+        name = nameParam;
+        gamesWon = 0;
+        gamesLost = 0;
+        setsWon = 0;
+        setsLost = 0;
+    }
+    public String getName(){
+        return name;
+    }
+    public void addGame(boolean won){
+        if(won){
+            gamesWon++;
+            if(gamesWon>=6){
+                if(gamesWon >= gamesLost+2){
+                    gamesWon = 0;
+                    gamesLost = 0;
+                    setsWon++;
+                }
+            }
+        }else{
+            gamesLost++;
+            if(gamesLost>=6){
+                if(gamesLost >= gamesWon+2){
+                    gamesWon = 0;
+                    gamesLost = 0;
+                    setsLost++;
+                }
+            }
+        }
+    }
+    public int getGamesWon(){
+        return gamesWon;
+    }
+    public int getGamesLost(){
+        return gamesLost;
+    }
+    public int getSetsWon(){
+        return setsWon;
+    }
+    public int getSetsLost(){
+        return setsLost;
+    }
+}
